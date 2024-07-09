cd979459a8e4fd0b71cb044f83543ff789bde7ad
T Saket Hatwar <hatwar.saket23@gmail.com>
2024-07-09 23:54:28 +0530

    accounts fixed

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,21 @@
 import {Hono} from "hono"
+import {db} from "@/db/drizzle";
+import {accounts} from "@/db/schema"
 
-const app = new Hono().get("/",(c)=>{
-    return c.json({accounts:[]})
-});
+ const app = new Hono()
+//     .get("/",async (c)=>{
+//     const data= db
+//     .select({
+//         id: accounts.id,
+//         name: accounts.name
+//     })
+//     .from(accounts)    
+//     return c.json({data})
+// });
+
+app.get("/",(c)=>{
+    return c.json({accounts:[]});
+})
 
 export default app;
 
