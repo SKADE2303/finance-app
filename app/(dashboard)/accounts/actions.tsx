c79079b9e65eeb299fff9c734e1312b47d2bb08b
T Saket Hatwar <hatwar.saket23@gmail.com>
2024-08-03 19:14:15 +0530

    Started working on catergories section

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -8,7 +8,9 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+import { useConfirm } from "@/hooks/use-confirm";
 
 type Props = {
     id: string,
@@ -16,12 +18,27 @@ type Props = {
 
 export const Actions = ({id}: Props) =>{
     
+    const deleteMutation = useDeleteAccount(id);
     const {onOpen}  = useOpenAccount();
+    const [ConfirmDialog,confim] = useConfirm(
+        "Are you sure?",
+        "You are about to delete this transaction."
+    )
+
+
+    const handleDelete = async () =>{
+        const ok = await confirm();
+
+        if(ok){
+            deleteMutation.mutate();
+        }
+    };
 
 
     
     return (
         <>
+            <ConfirmDialog></ConfirmDialog>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant="ghost" className="size-8 p-0">
@@ -30,12 +47,21 @@ export const Actions = ({id}: Props) =>{
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuItem
-                    disabled={false}
+                    disabled={deleteMutation.isPending}
                     onClick ={()=>onOpen(id)}
                     >
                         <Edit className="size-4 mr-2" />
                         Edit
                     </DropdownMenuItem>
+
+                    <DropdownMenuItem
+                    disabled={deleteMutation.isPending}
+                    onClick ={handleDelete}
+                    >
+                        <Trash className="size-4 mr-2" />
+                        Delete
+                    </DropdownMenuItem>
+
                 </DropdownMenuContent>
             </DropdownMenu>
         </>
